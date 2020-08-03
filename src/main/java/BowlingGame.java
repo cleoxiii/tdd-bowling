b2119dcba718ee4f7f1de4a96f681d73bc0106df
Yun Chenxi <cleoxiii@163.com>
2020-08-04 00:54:41 +0800

    [yunchenxi] feat: should calculate total score when exist spare among frames.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,8 +1,19 @@
-import java.util.Arrays;
-
 public class BowlingGame {
+    private static final int FULL_SCORE = 10;
+
     public int calculateTotalScore(int[] scores) {
-        int totalScore = Arrays.stream(scores).reduce(Integer::sum).getAsInt();
+        int totalScore = 0;
+        int index = 0;
+        for (int frame = 0; frame < 10; ++frame) {
+            int scoreOfSingleFrame = scores[index] + scores[index + 1];
+            if ( scoreOfSingleFrame == 10) {
+                totalScore += FULL_SCORE + scores[index + 2];
+            } else {
+                totalScore += scoreOfSingleFrame;
+            }
+            index += 2;
+        }
+
         return totalScore;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -21,4 +21,35 @@ public class BowlingGameTest {
 
         assertEquals(30, totalScore);
     }
+
+
+    @Test
+    void should_calculate_total_score_when_exist_spare_normally_in_first_nine_frames() {
+        int[] scores = {1, 9, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        BowlingGame game = new BowlingGame();
+
+        int totalScore = game.calculateTotalScore(scores);
+
+        assertEquals(14, totalScore);
+    }
+
+    @Test
+    void should_calculate_total_score_when_exist_spare_normally_in_first_nine_frames_continued() {
+        int[] scores = {1, 9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+        BowlingGame game = new BowlingGame();
+
+        int totalScore = game.calculateTotalScore(scores);
+
+        assertEquals(86, totalScore);
+    }
+
+    @Test
+    void should_calculate_total_score_when_exist_spare_in_last_frame() {
+        int[] scores = {1, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 8, 2};
+        BowlingGame game = new BowlingGame();
+
+        int totalScore = game.calculateTotalScore(scores);
+
+        assertEquals(18, totalScore);
+    }
 }
